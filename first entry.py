3771b7f7823eb2f395b4ee96ff902cf0ee2b987a
slavichowen <slavichowen@gmail.com>
2025-10-19 20:58:47 -0700

    Session 1 Prompt for Coding Journal #1

---
+++ first entry.py
@@ -0,0 +1,3 @@
+print("Owen Slavich")
+print("He/Him")
+print("My favorite movie is The Shawshank Redemption because I used to watch it with my grandma. My favorite food is orange chicken from panda express.")
