69fc3b16a71042865ef8d40c49f9099eb4afd8a6
slavichowen <slavichowen@gmail.com>
2025-10-19 21:12:44 -0700

    Session 2 prompt for Coding Journal #1

---
+++ entry 2.py
@@ -0,0 +1,16 @@
+float1 = float(input("Enter a floating point number: "))
+float2 = float(input("Enter a floating point number: "))
+r1 = float1 + float2
+print("Result", r1, "Type", type(r1))
+
+int1 = int(input("Enter an integer: "))
+int2 = int(input("Enter an integer: "))
+r2 = int1 - int2
+print("Result", r2, "Type", type(r2))
+
+float3 = float(input("Enter a floating point number: "))
+int3 = int(input("Enter an integer: "))
+r3 = int3 * float3
+print("Result", r3, "Type", type(r3))
+
+input("enter to close")
